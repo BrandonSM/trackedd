a7ae51ed258a0696c3fda5046dcd0513efbc061d
BrandonSM <brandon@skeptikalmedia.com>
2016-01-19 20:20:59 -0500

    Added bootstrap js

--- app/index.php
+++ app/index.php
@@ -6,6 +6,10 @@
 <!-- latest compiled and minified css -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+<!-- latest compiled and minified js -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
   </head>
 
   <body>
