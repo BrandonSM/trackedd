29d771dd4c679957aae37d0d2d326486083e00c0
BrandonSM <brandon@skeptikalmedia.com>
2016-01-19 20:22:31 -0500

    moved bootstrap code

--- app/index.php
+++ app/index.php
@@ -6,16 +6,18 @@
 <!-- latest compiled and minified css -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<!-- latest compiled and minified js -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-
   </head>
 
   <body>
 
   <h1>Trackedd</h1>
   <h3>Simple.Time.Tracking</h3>
+
+
+
+
+<!-- latest compiled and minified js -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
   </body>
 
