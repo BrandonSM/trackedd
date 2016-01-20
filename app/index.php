5b50e20bbef5aec00111cc70a035e0a46348d91f
BrandonSM <brandon@skeptikalmedia.com>
2016-01-19 21:09:47 -0500

    Added the template

--- app/index.php
+++ app/index.php
@@ -6,15 +6,27 @@
     <!-- latest compiled and minified css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+    <!-- load font awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   </head>
 
   <body>
 
-    <h1>Trackedd</h1>
-    <h3>Simple.Time.Tracking</h3>
+    <!-- application container -->
+    <div class="container-flud">
+      <h1>Trackedd</h1>
+      <h3>Simple.Time.Tracking</h3>
 
+      <header class="row">
+        <div class="col-xs-6">
+          <a href="#">Enter Restore Mode</a>
+        </div>
+        <div class="col-xs-6">
+          Total Time: <a href="#"></a>
+        </div>
+      </header>
 
-
+    </div>
 
     <!-- load jquery -->
     <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
