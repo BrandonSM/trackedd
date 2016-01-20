bd90f2aaf1720e3e57f5c06c9e4e4912a94cd461
BrandonSM <brandon@skeptikalmedia.com>
2016-01-19 20:18:58 -0500

    Added bootstrap

--- app/index.php
+++ app/index.php
@@ -2,11 +2,16 @@
 <html>
   <head>
     <title>Trackedd</title>
+
+<!-- latest compiled and minified css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
   </head>
 
   <body>
 
-    Simple time tracker application -- Trackedd
+  <h1>Trackedd</h1>
+  <h3>Simple.Time.Tracking</h3>
 
   </body>
 
