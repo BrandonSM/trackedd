047cd6a679ed63ded11adc666a822a934124b12c
BrandonSM <brandon@skeptikalmedia.com>
2016-01-19 20:30:59 -0500

    Added jquery

--- app/index.php
+++ app/index.php
@@ -3,21 +3,24 @@
   <head>
     <title>Trackedd</title>
 
-<!-- latest compiled and minified css -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- latest compiled and minified css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
   </head>
 
   <body>
 
-  <h1>Trackedd</h1>
-  <h3>Simple.Time.Tracking</h3>
+    <h1>Trackedd</h1>
+    <h3>Simple.Time.Tracking</h3>
 
 
 
 
-<!-- latest compiled and minified js -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- load jquery -->
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+
+    <!-- latest compiled and minified js -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
   </body>
 
