a7d0db5c2d0c828b1c232343880df02f23cdce4e
BrandonSM <brandon@skeptikalmedia.com>
2016-01-20 20:15:49 -0500

    Added the inputs

--- app/index.php
+++ app/index.php
@@ -18,14 +18,28 @@
       <h3>Simple.Time.Tracking</h3>
 
       <header class="row">
+
         <div class="col-xs-6">
           <a href="#">Enter Restore Mode</a>
         </div>
-        <div class="col-xs-6">
+        <div class="col-xs-6 text-right">
           Total Time: <a href="#"></a>
         </div>
       </header>
 
+      <div class="row">
+        <form>
+          <div class="col-xs-10">
+           <input class="form-control" placeholder="What're you up to?">
+          </div>
+          <div class="col-xs-2">
+            <button type="submit" class="btn btn-block btn-success">Start</button>
+          </div>
+        </form>
+      </div>
+
+      <hr>
+    <table class="table table-bordered"></table>
     </div>
 
     <!-- load jquery -->
@@ -35,6 +49,4 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
   </body>
-
-
 </html>
