8d12ccde9c763e49b06b12ab20d5e55d5847887b
BrandonSM <brandon@skeptikalmedia.com>
2016-01-22 20:07:40 -0500

    Gave it a little color and changed the font

--- app/index.php
+++ app/index.php
@@ -3,9 +3,13 @@
 <html>
   <head>
     <title>Trackedd</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
 
     <!-- latest compiled and minified css -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
     <!-- load font awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -14,7 +18,7 @@
   <body>
 
     <!-- application container -->
-    <div class="container-flud">
+    <div class="container-fluid">
       <h1>Trackedd</h1>
       <h3>Simple.Time.Tracking</h3>
 
@@ -47,8 +51,8 @@
           <td><h1>TASK NAME</h1><span style="float:left;width:50%;">Start Time</span><span style="float:right;width:50%;">Stop Time</span><div style="clear:both;"></div></td>
           <td>Data</td>
           <td>Data</td>
-          <td><button class="btn btn-primary"><?php echo(i('stop')); ?></button></td>
-          <td><button class="btn btn-red"><?php echo(i('times')); ?></button></td>
+          <td><button class="btn btn-lg btn-primary"><?php echo(i('stop')); ?></button></td>
+          <td><button class="btn btn-lg btn-red"><?php echo(i('times')); ?></button></td>
         </tr>
       </tbody>
 
